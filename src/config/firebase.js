35b8dd62e1b05150e7de7802f29ac7b83b7afc55
gravo0801 <fnaticdoc@gmail.com>
2026-03-22 23:43:07 +0900

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,13 @@ import { getAuth } from 'firebase/auth';
 
 // Firebase 설정 - 본인의 Firebase 프로젝트 정보로 변경하세요
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBSj12rE7VM5cuS25E_xe7TvGKFtfTq0BI",
+  authDomain: "recipelog-b2160.firebaseapp.com",
+  projectId: "recipelog-b2160",
+  storageBucket: "recipelog-b2160.firebasestorage.app",
+  messagingSenderId: "157595326727",
+  appId: "1:157595326727:web:fbae950bf42737e2e4cda2",
+  measurementId: "G-SEJRKQJF1Q"
 };
 
 // Firebase 초기화
